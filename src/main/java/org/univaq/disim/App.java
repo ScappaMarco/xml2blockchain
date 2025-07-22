d265bae8fc3874d8e704aefd21714b00da9e6a2e
ScappaMarco <mscappa0@gmail.com>
2025-07-22 19:39:38 +0200

    initial commit

---
+++ src/main/java/org/univaq/disim/App.java
@@ -0,0 +1,13 @@
+package org.univaq.disim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
