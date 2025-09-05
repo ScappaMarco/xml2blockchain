63e48aeb589e5908228276ccafe262f527af6d90
ScappaMarco <mscappa0@gmail.com>
2025-09-05 17:24:32 +0200

    blockchain implementation started

--- src/main/java/cbp/src/history/ModelHistory.java
+++
@@ -1,4 +0,0 @@
-package cbp.src.history;
-
-public class ModelHistory {
-}
--- src/main/java/loader/src/Loader.java
+++ src/main/java/loader/src/Loader.java
@@ -40,4 +40,8 @@ public class Loader {
 
          */
     }
+
+    public static void mapInBlockchain(ChangeEventsMap changeEventsMap) {
+        //...
+    }
 }
