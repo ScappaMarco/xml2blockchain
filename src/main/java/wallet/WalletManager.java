5d67822b7d3ce99e0ae638fe1333e33b0eab4572
ScappaMarco <mscappa0@gmail.com>
2025-09-05 17:30:50 +0200

    bc started

---
+++ src/main/java/wallet/WalletManager.java
@@ -0,0 +1,15 @@
+package wallet;
+
+import org.hyperledger.fabric.gateway.Identities;
+import org.hyperledger.fabric.gateway.Identity;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.PrivateKey;
+
+public class WalletManager {
+
+}
