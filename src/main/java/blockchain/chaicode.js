31ae19250db26a3ea255a659939cdfbab4eb45d0
ScappaMarco <mscappa0@gmail.com>
2025-09-13 15:52:38 +0200

    chaincode js added

---
+++ src/main/java/blockchain/chaicode.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class BlockContract extends Contract {
+
+    async CreateBlock(ctx, blockId, jsonData) {
+        const exists = await ctx.stub.getState(blockId);
+        if (exists && exists.length > 0) {
+            throw new Error(`The Block with this ID: ${blockId} alredy exists`);
+        }
+        await ctx.stub.putState(blockId, Buffer.from(jsonData));
+        return `The Block with this ID:  ${blockId} has been created successfuly`;
+    }
+
+    async ReadBlock(ctx, blockId) {
+        const blockBytes = await ctx.stub.getState(blockId);
+        if (!blockBytes || blockBytes.length === 0) {
+            throw new Error(`Block with this ID: ${blockId} does not exists`);
+        }
+        return blockBytes.toString();
+    }
+}
+
+module.exports = BlockContract;
