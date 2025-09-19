dfb270430b1fbbffe6dc011632ff5aaf6e41a9d2
ScappaMarco <mscappa0@gmail.com>
2025-09-19 16:50:54 +0200

    adding python scripts to timer the program

---
+++ scripts/timer.py
@@ -0,0 +1,80 @@
+import pandas
+import statistics
+import subprocess
+import re
+
+def run_java_benchmark(contact_name, runs):
+    print("IN THE METHOD")
+    """
+    Esegue il benchmark per un contratto specifico
+    :param contact_name:
+    :return:
+    """
+    try:
+        cmd = [
+            "mvn", "-q", "compile", "exec:java", f"-DcontractName={contact_name}"
+        ]
+
+        parsing_times = []
+        serializing_times = []
+        bc_saving_times = []
+        general_times = []
+
+        print("AT THE START OF THE FOR LOOP")
+        for i in range(runs):
+            print("IN THE FOR LOOP")
+            print(f"ATTENSTION: starting run number {i}")
+            result = subprocess.run(cmd, capture_output=True, text=True)
+            if result.returncode != 0:
+                print(f"ERROR - PARSING TIME: errore occurred running {contact_name}: {result.stderr}")
+                return None
+            else:
+                print("RETURN CODE OK")
+                for line in result.stdout.splitlines():
+                    if "TIME RECORD - PARSING TIME" in line:
+                        parsing_time = re.search(r'(\d+)ms', line)
+                        print(f"FOUND A PARSING TIME LINE - {parsing_time}")
+                        if parsing_time:
+                            parsing_times.append(int(parsing_time.group(1)))
+                    elif "TIME RECORD - SERIALIZATION TIME" in line:
+                        serialization_time = re.search(r'(\d+)ms', line)
+                        print(f"FOUND A SERIALIZATION TIME LINE - {serialization_time}")
+                        if serialization_time:
+                            serializing_times.append(int(serialization_time.group(1)))
+                    elif "TIME RECORD - BLOCKCHAIN TIME" in line:
+                        blockchain_time = re.search(r'(\d+)ms', line)
+                        print(f"FOUND A BLOCKCHAIN SAVING TIME LINE - {blockchain_time}")
+                        if blockchain_time:
+                            bc_saving_times.append(int(blockchain_time.group(1)))
+                    else:
+                        if "TIME RECORD - GENERAL TIME" in line:
+                            general_time = re.search(r'(\d+)ms', line)
+                            print(f"FOUND A GENARAL TIME LINE - {general_time}")
+                            if general_time:
+                                general_times.append(int(general_time.group(1)))
+
+        means = {
+            "parsing mean value": statistics.mean(parsing_times) if parsing_times else None,
+            "serialization mean value": statistics.mean(serializing_times) if serializing_times else None,
+            "blockchain saving mean time": statistics.mean(bc_saving_times) if bc_saving_times else None,
+            "general mean time": statistics.mean(general_times) if general_times else None
+        }
+
+        return means
+
+    except Exception as e:
+        print(f"ERROR - PARSING TIME: exception for contract {contact_name}: {e}")
+
+def main():
+    contracts = ["block-contract-dc", "block-contract"]
+    res = []
+
+    for contract in contracts:
+        print(f"CONTRACT {contract}: testing")
+
+        results = run_java_benchmark(contract, 5)
+        if results:
+            res.append(results)
+
+if __name__ == "__main__":
+    main()
